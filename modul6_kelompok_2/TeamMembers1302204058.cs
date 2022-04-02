5ab7010855ca4e7c3e763da87fbb80a802982c82
devadelt <devaraja@student.telkomuniversity.ac.id>
2022-04-02 12:27:14 +0700

    menambahkan class TeamMembers1302204058

---
+++ modul6_kelompok_2/TeamMembers1302204058.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+
+namespace modul6_kelompok_2
+{
+    public class TeamMembers1302204058
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public int nim { get; set; }
+
+        public input() { }
+
+        public static void ReadJSON()
+        {
+            //Baca file Json yang diinginkan
+            String jsonString = File.ReadAllText("Users/devar/source/repos/jMOD6/modul6_kelompok_2/jurnal6_2_1302204058.json");
+            //Deserialize file json menjadi objectS
+            dynamic input = JsonConvert.DeserializeObject(jsonString);
+            Console.WriteLine("Team member list:");
+
+        }
+
+        }
+    }
+}
