dd4484a13ada2bdd113d0fe07f9278718594c460
devadelt <devaraja@student.telkomuniversity.ac.id>
2022-04-02 12:09:35 +0700

    menambahkan class DataMahasiswa1302204058

---
+++ modul6_kelompok_2/DataMahasiswa1302204058.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+
+namespace modul6_kelompok_2
+{
+    public class DataMahasiswa1302204058
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public Address address { get; set; }
+        public Course course { get; set; }
+
+        public input() { } 
+
+        public static void ReadJSON()
+        {
+            //Baca file Json yang diinginkan
+            String jsonString = File.ReadAllText("Users/devar/source/repos/jMOD6/modul6_kelompok_2/jurnal6_1_1302204058.json");
+            //Deserialize file json menjadi objectS
+            dynamic input = JsonConvert.DeserializeObject(jsonString);
+
+            Console.WriteLine("Nama: " + input.firstName + input.lastName);
+            Console.WriteLine("Jenis Kelamin: " + input.gender);
+            Console.WriteLine("Umur: " + input.age);
+            Console.WriteLine("Alamat: " + input.address.streetAddress + input.address.city + input.address.state);
+            Console.WriteLine("Mata Kuliah: ");
+            foreach (var item in input.courses)
+            {
+                Console.Write("Kode Kelas: " + item.code + ", ");
+                Console.WriteLine("Nama Kelas: " + item.name);
+            }
+
+        }
+    }
+
+    public class Address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+}
--- modul6_kelompok_2/Program.cs
+++ modul6_kelompok_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+
+namespace modul6_kelompok_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa1302204058.ReadJSON();
+        }
+    }
+}
